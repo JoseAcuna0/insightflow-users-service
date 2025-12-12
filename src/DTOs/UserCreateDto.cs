3db56c9365075f4c997fc0e80f91e83e54a18d44
JoseAcuna0 <jose.acuna@alumnos.ucn.cl>
2025-12-11 21:17:58 -0300

    feat: added User model and Dtos (Create, Response, Update)

---
+++ src/DTOs/UserCreateDto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace users_service.src.DTOs
+{
+    public class UserCreateDto
+    {
+    
+    public string FullName { get; set; } = string.Empty;
+    
+    public string Email { get; set; } = string.Empty;
+    
+    public string Username { get; set; } = string.Empty;
+    
+    public string Password { get; set; } = string.Empty;
+    
+    public DateOnly DateOfBirth { get; set; }
+    
+    public string Address { get; set; } = string.Empty;
+    
+    public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
---
+++ src/DTOs/UserResponseDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace users_service.src.DTOs
+{
+    public class UserResponseDto
+    {
+        public Guid Id { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Username { get; set; } = string.Empty;
+
+        public string UserStatus { get; set; } = string.Empty;
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string Address { get; set; } = string.Empty;
+    }
+}
