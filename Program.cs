df90d930edd0a51e5281aa2dd746906045100b8c
JoseAcuna0 <jose.acuna@alumnos.ucn.cl>
2025-12-12 16:33:44 -0300

    feat: added CORS to connection to frontend

--- Program.cs
+++ Program.cs
@@ -6,27 +6,39 @@ using users_service.src.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers(); 
 
+
+builder.Services.AddControllers(); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IUserService, UserService>(); 
 
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
+
 
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
- 
-    app.UseSwagger();
-    app.UseSwaggerUI(); 
-}
+app.UseSwagger();
+app.UseSwaggerUI(); 
+
 
 app.UseHttpsRedirection();
 
+app.UseCors();
 
 app.MapControllers(); 
 
